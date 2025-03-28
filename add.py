2f35975acf6f09e1afadeb468fc8914e81b21b0a
Vitor Petri <diismas19@gmail.com>
2025-03-28 20:51:33 -0300

    Update add.py

--- add.py
+++ add.py
@@ -5,4 +5,4 @@ if len(sys.argv) != 3:
     print('Usage: python3 add.py <path_of_file_to_be_added> <path_of_file_to_add>')
     sys.exit(1)
 
-add_files(sys.argv[-2],sys.argv[-1])
+add_files(sys.argv[1],sys.argv[2])
